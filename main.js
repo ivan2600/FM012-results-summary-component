bc03984edfee84aa21a58703f77c04f439e1a8f0
Ivan Adamo <ivan.adamo.8@gmail.com>
2024-01-07 10:42:39 -0300

    json learned jeje

---
+++ main.js
@@ -0,0 +1,32 @@
+fetch('data.json')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log(data);
+    document.getElementById('category').innerText = data[0].icon;
+
+    const iconsContainer = document.getElementById('icons-container');
+
+    data.forEach(jsonData => {
+      const iconElement = document.createElement('img');
+      iconElement.src = jsonData.icon;
+      iconElement.alt = jsonData.category;
+      iconElement.title = `${jsonData.category} - Score: ${jsonData.score}`;
+      iconsContainer.appendChild(iconElement);
+      const categoryElement = document.createElement('p');
+      categoryElement.classList.add('category')
+      categoryElement.innerHTML = jsonData.category;
+      iconsContainer.appendChild(categoryElement);
+      const scoreElement = document.createElement('p');
+      scoreElement.classList.add('score');
+      scoreElement.innerHTML = jsonData.score + " / 100";
+      iconsContainer.appendChild(scoreElement);
+    });
+  })
+  .catch(error => {
+    console.error('Error fetching data:', error);
+  });
