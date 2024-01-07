2b7d85df548bc184251da457231aa7f8dee4938f
Ivan Adamo <ivan.adamo.8@gmail.com>
2024-01-07 16:21:48 -0300

    mobile desing finished

--- main.js
+++ main.js
@@ -11,19 +11,22 @@ fetch('data.json')
     const iconsContainer = document.getElementById('icons-container');
 
     data.forEach(jsonData => {
+      const divElement = document.createElement('div');
+      divElement.classList.add(`${jsonData.category}`);
+      iconsContainer.appendChild(divElement);
       const iconElement = document.createElement('img');
       iconElement.src = jsonData.icon;
       iconElement.alt = jsonData.category;
       iconElement.title = `${jsonData.category} - Score: ${jsonData.score}`;
-      iconsContainer.appendChild(iconElement);
+      divElement.appendChild(iconElement);
       const categoryElement = document.createElement('p');
       categoryElement.classList.add('category')
       categoryElement.innerHTML = jsonData.category;
-      iconsContainer.appendChild(categoryElement);
+      divElement.appendChild(categoryElement);
       const scoreElement = document.createElement('p');
       scoreElement.classList.add('score');
-      scoreElement.innerHTML = jsonData.score + " / 100";
-      iconsContainer.appendChild(scoreElement);
+      scoreElement.innerHTML = jsonData.score + `<span> / 100</span>`;
+      divElement.appendChild(scoreElement);
     });
   })
   .catch(error => {
